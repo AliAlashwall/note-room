f97b0e50f0994379acb7eada836fd22829d0d403
Ali <alialashwall14@gmail.com>
2024-08-20 19:33:36 +0300

    Separate note item in an other file

--- app/src/main/java/com/example/testroomdatabase/ui/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/example/testroomdatabase/ui/screens/notes/NotesScreen.kt
@@ -2,18 +2,13 @@ package com.example.testroomdatabase.ui.screens.notes
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,11 +19,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.testroomdatabase.ui.screens.notes.component.NoteItem
 import com.example.testroomdatabase.ui.theme.TestRoomDatabaseTheme
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -61,35 +55,11 @@ fun NotesScreen(viewModel: NoteViewModel) {
         }
         LazyColumn {
             items(notesUiState) { note ->
-                Card(
-                    colors = CardDefaults.cardColors(Color.LightGray),
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    onClick = { viewModel.deleteNote(note) }
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(26.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = note.content,
-                            textAlign = TextAlign.Start,
-                            modifier = Modifier
-                                .padding(16.dp)
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        Text(
-                            text = note.date.toString().take(10),
-                            textAlign = TextAlign.End,
-                            modifier = Modifier
-                                .padding(16.dp)
-                        )
-                    }
-                }
+                NoteItem(
+                    onClickItem = { viewModel.deleteNote(note) },
+                    content = note.content,
+                    date = note.date.toString().substringBefore("GMT")
+                )
             }
         }
     }
---
+++ app/src/main/java/com/example/testroomdatabase/ui/screens/notes/component/NoteItem.kt
@@ -0,0 +1,69 @@
+package com.example.testroomdatabase.ui.screens.notes.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testroomdatabase.ui.theme.TestRoomDatabaseTheme
+
+@Composable
+fun NoteItem(
+    onClickItem: () -> Unit,
+    content: String,
+    date: String
+) {
+    Card(
+        colors = CardDefaults.cardColors(Color.White),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.elevatedCardElevation(4.dp),
+        onClick = onClickItem,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Text(
+                text = content,
+                textAlign = TextAlign.Start,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth(),
+            )
+            Text(
+                text = date,
+                textAlign = TextAlign.End,
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = Color.Gray,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+private fun NoteItemPreview() {
+    TestRoomDatabaseTheme {
+        NoteItem(
+            onClickItem = {},
+            content = "This is a note",
+            date = "2021-09-01"
+        )
+    }
+}
